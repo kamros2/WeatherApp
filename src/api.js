d1520cf65cd07da000773f883c42752cc66ff02c
kamros2 <98050427+kamros2@users.noreply.github.com>
2022-08-23 10:06:27 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -10,4 +10,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
 
-export const WEATHER_API_KEY = "c9d2217340c15e58d350b486ec9403e6"
+export const WEATHER_API_KEY = "PASTE YOUR WEATHER API KEY HERE"
