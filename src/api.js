78531fff320bbdf8442b38beedd2426eacce5a91
kamros2 <kamil.ros@gmail.com>
2022-08-23 02:08:33 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ecb1c131dbmsh27f0a0b6480980cp1477fbjsn48a7d6fc2351',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "c9d2217340c15e58d350b486ec9403e6"
